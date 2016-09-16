50968c96a7c5c64834bb7b4a1fd8fe09396c94ef
Dulcecu <atm199090@gmail.com>
2016-09-16 19:29:02 +0200

    Update Comparable.java

--- src/proyecto/Comparable.java
+++ src/proyecto/Comparable.java
@@ -1,84 +1,68 @@
 package proyecto;
-import java.lang.Comparable;
 
 /**
- * Created by Dulcecu on 14/09/2016.
+ * Created by Turpitude on 14/09/2016.
  */
 
-abstract class Figuras implements Comparable<Figuras>
-{
-
-    public abstract double area();
-
-    //Condiciones ordenadoras para que pueda utilizarla el Arrays.Sort
-    
-    public int compareTo(Figuras f)
-    {
-        if(this.area()<f.area())
+ interface Figuras
         {
-            return -1;
+
+            public double area();
         }
-        if(this.area()>f.area())
-        {
-            return 1;
-        }
-        return 0;
-    }
 
 
 
-}
-class rectangulo extends Figuras {
-    double l1, l2;
+
+
+class rectangulo implements  Figuras {
+    public double l1, l2;
+
     public rectangulo(double l1, double l2) {
         this.l1 = l1;
-        this.l2=l2;
-    }
-    public double area(){
-
-        return this.l1*this.l2;
+        this.l2 = l2;
     }
 
-    @Override
-    public int compareTo(Figuras f) {
-        return super.compareTo(f);
+    public double area() {
+
+        return this.l1 * this.l2;
     }
 }
+
+
 class cuadrado extends rectangulo {
     public cuadrado(double l){
         super (l,l);
     }
 }
-class triangulo extends Figuras{
-    double b,h;
-    public triangulo(double b, double h){
-        this.b=b;
-        this.h=h;
-    }
-    public double area(){
+class triangulo implements Figuras {
+    public double b, h;
 
-        return (this.b*this.h/2);
+    public triangulo(double b, double h) {
+        this.b = b;
+        this.h = h;
     }
-
-    @Override
-    public int compareTo(Figuras f) {
-        return super.compareTo(f);
-    }
-}
-class circulo extends Figuras{
-    double radio;
-    public circulo(double radio){
-        this.radio = radio;
-    }
-
 
     public double area() {
 
-        return Math.PI*this.radio *this.radio;
-    }
-
-    @Override
-    public int compareTo(Figuras f) {
-        return super.compareTo(f);
+        return (this.b * this.h / 2);
     }
 }
+
+
+    class circulo implements Figuras {
+        public double radio;
+
+        public circulo(double radio) {
+            this.radio = radio;
+        }
+
+
+        public double area() {
+
+            return Math.PI * this.radio * this.radio;
+        }
+    }
+
+
+
+
