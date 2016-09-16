bc13babeb1a929bd8d6e297e4156e247561eacb9
Dulcecu <atm199090@gmail.com>
2016-09-16 12:18:17 +0200

    Initial commit

---
+++ src/proyecto/Comparable.java
@@ -0,0 +1,84 @@
+package proyecto;
+import java.io.*;
+import java.lang.Comparable;
+
+/**
+ * Created by Turpitude on 14/09/2016.
+ */
+
+abstract class Figuras implements Comparable<Figuras>,Serializable
+{
+
+    public abstract double area();
+
+
+    public int compareTo(Figuras f)
+    {
+        if(this.area()<f.area())
+        {
+            return -1;
+        }
+        if(this.area()>f.area())
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+
+
+}
+class rectangulo extends Figuras {
+    double l1, l2;
+    public rectangulo(double l1, double l2) {
+        this.l1 = l1;
+        this.l2=l2;
+    }
+    public double area(){
+
+        return this.l1*this.l2;
+    }
+
+    @Override
+    public int compareTo(Figuras f) {
+        return super.compareTo(f);
+    }
+}
+class cuadrado extends rectangulo {
+    public cuadrado(double l){
+        super (l,l);
+    }
+}
+class triangulo extends Figuras{
+    double b,h;
+    public triangulo(double b, double h){
+        this.b=b;
+        this.h=h;
+    }
+    public double area(){
+
+        return (this.b*this.h/2);
+    }
+
+    @Override
+    public int compareTo(Figuras f) {
+        return super.compareTo(f);
+    }
+}
+class circulo extends Figuras{
+    double radio;
+    public circulo(double radio){
+        this.radio = radio;
+    }
+
+
+    public double area() {
+
+        return Math.PI*this.radio *this.radio;
+    }
+
+    @Override
+    public int compareTo(Figuras f) {
+        return super.compareTo(f);
+    }
+}
---
+++ src/proyecto/Main.java
@@ -0,0 +1,56 @@
+package proyecto;
+import java.io.*;
+import java.util.Arrays;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Figuras[] ll = new Figuras[4];
+        ll[0]=new rectangulo(2,7);
+        ll[1]=new cuadrado(10);
+        ll[2]=new circulo(8);
+        ll[3]=new triangulo(6,9);
+        double suma=0;
+        for (Figuras f:ll){
+            suma=suma+f.area();
+
+        }
+        System.out.print(suma+"//////");
+        Arrays.sort(ll);
+
+        for(Figuras f:ll)
+        {
+
+            System.out.print(f.area()+",");
+
+        }
+
+
+
+        File file = new File("c:/Users/Turpitude/Desktop/fichero.bin");
+        FileOutputStream fos = new FileOutputStream(file);
+        FileInputStream fis = new FileInputStream("c:/Users/Turpitude/Desktop/fichero.bin");
+        ObjectOutputStream out = new ObjectOutputStream(fos);
+        ObjectInputStream in = new ObjectInputStream(fis);
+        //Convertir objeto a byte y meter en fichero
+        out.writeObject(ll);
+        //Convertir bytes y recuperar objeto
+        Figuras[] llrecuperar= new Figuras[4];
+        try {
+            llrecuperar=(Figuras[])in.readObject();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for(Figuras f:llrecuperar)
+        {
+
+            System.out.print(f.area());
+
+        }
+
+
+    }
+
+}
